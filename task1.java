b910901aac525b21bec5b90df217677fa007dd3a
Alexei <alexei@alexey.com>
2023-03-25 22:45:29 +0300

    zatup

--- task1.java
+++ task1.java
@@ -8,15 +8,27 @@ public class task1 {
 
         LinkedList<Integer> listlink = new LinkedList<>();
 
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < 10; i++) {
             listlink.add((int) (Math.random() * 100));
         }
         System.out.println(listlink);
+        
+        System.out.println(list(listlink));
 
-        for (int i = 0; i < listlink.size(); i++) {
-            listlink.addFirst(listlink.get(i));
-            listlink.remove(i + 1);
-        }
-        System.out.println(listlink);
+        // for (int i = 0; i < listlink.size(); i++) {
+        //     listlink.addFirst(listlink.get(i));
+        //     listlink.remove(i + 1);
+        // }
+        // System.out.println(listlink);
     }
+
+    static int [] list(LinkedList<Integer> list) {
+        for (int i = 0; i < list.size(); i++) {
+            list.addFirst(list.get(i));
+            list.remove(i + 1);
+        }
+
+        return list(list);
+    }
+        
 }
