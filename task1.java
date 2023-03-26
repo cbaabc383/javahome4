f267d562be001759645a0423c14d2e90554f552a
Alexei <alexei@alexey.com>
2023-03-26 09:17:38 +0300

    task1Finish

--- task1.java
+++ task1.java
@@ -8,27 +8,19 @@ public class task1 {
 
         LinkedList<Integer> listlink = new LinkedList<>();
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 20; i++) {
             listlink.add((int) (Math.random() * 100));
         }
         System.out.println(listlink);
         
         System.out.println(list(listlink));
-
-        // for (int i = 0; i < listlink.size(); i++) {
-        //     listlink.addFirst(listlink.get(i));
-        //     listlink.remove(i + 1);
-        // }
-        // System.out.println(listlink);
     }
 
-    static int [] list(LinkedList<Integer> list) {
+    static LinkedList<Integer> list (LinkedList<Integer> list) {
         for (int i = 0; i < list.size(); i++) {
             list.addFirst(list.get(i));
             list.remove(i + 1);
         }
-
-        return list(list);
+        return list;
     }
-        
 }
